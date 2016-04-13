4eddc880443b176c804513854519c2506c671b90
freddy <freddy.marechal@gmail.com>
2016-04-13 16:52:32 +0100

    feat: Modification following review 2
    Correction of the following bugs in file - js/utilities/googleMapGenerator.js
    1. Use of defer and Async in function loadGoogleMap()
    2. Animate the marker when it's clicked
    3. loadArticles() is now using knockout MVVM instead of JQuery

--- js/utilities/googleMapGenerator.js
+++ js/utilities/googleMapGenerator.js
@@ -4,11 +4,25 @@ var headingImageView = [5, 235, 55, 170, 190, 240, -10, 10, 190];
 var streetViewImage;
 var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=180x90&location=';
 
+//Alert the user in case of an error when loading the google map (called by loadGoogleMap())
+function googleErrorFunction() {
+    alert('The google map could not be loaded...');
+}
+
 //load the map via the initialise() function
 function loadGoogleMap() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false&callback=initialise';
+     //Async allows the script to be downloaded in the background without blocking.
+     //Then, the moment it finishes downloading, rendering is blocked and that script executes.
+     //Render resumes when the script has executed.
+    script.async = "async"
+     //Defer does the same thing, except claims to guarantee that scripts execute in the order they were specified on the page,
+     //and that they will be executed after the document has finished parsing.
+     // So, some scripts may finish downloading then sit and wait for scripts that downloaded later but appeared before them.
+    script.defer = "defer"
+    script.onerror = "googleErrorFunction()"
     document.body.appendChild(script);
 }
 
@@ -48,9 +62,27 @@ function getStreetViewImage(index) {
     streetViewImage = streetViewUrl + markersModel[index].lat + ',' + markersModel[index].lng + '&fov=75&heading=' + headingImageView[index] + '&pitch=10';
 }
 
+//Bound the marker when the user click it for 2s, then stop bouncing
+function toggleBounce(marker) {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        setTimeout(function () {
+            marker.setAnimation(null);
+        }, 2000);
+    }
+}
+
 //Locates  markers on the map
 //Sets infoWindows for each marker
 function setMarkers(location) {
+
+    infowindow = new google.maps.InfoWindow({
+        content: ""
+    });
+
     for (i = 0; i < location.length; i++) {
         location[i].holdMarker = new google.maps.Marker({
             position: new google.maps.LatLng(location[i].lat, location[i].lng),
@@ -79,13 +111,15 @@ function setMarkers(location) {
             location[i].city + '<br></p><a class="web-links" href="http://' + location[i].url +
             '" target="_blank">' + location[i].url + '</a>';
 
-        var infowindow = new google.maps.InfoWindow({
-            content: markersModel[i].contentString
-        });
+        infowindow.content =  markersModel[i].contentString;
+        infowindow.location = location;
 
         //Click marker to view infoWindow and zoom if need be...
         new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
             return function() {
+
+                toggleBounce(marker);
+
                 infowindow.setContent(location[i].contentString);
                 infowindow.open(map, this);
                 var windowWidth = $(window).width();
@@ -120,13 +154,11 @@ function setMarkers(location) {
 }
 
 //Wrapper function that contains all the ajax call to get articles on the map
-function getArticlesInformation(location) {
+function loadArticles(location) {
     //get the info from the different ajax calls
-    //getNyTimesInfo(location.city);
-    //getWikkiInfo(location.borough);
+    getNyTimesInfo(location.city);
+    getWikkiInfo(location.borough);
 
-    //viewModel.markersModel.city.subscribe(function(newValue) {getNyTimesInfo(newValue)});
-    //viewModel.markersModel.borough.subscribe(function(newValue) {getWikkiInfo(newValue)});
 }
 
 
--- js/viewModels/markerViewModel.js
+++ js/viewModels/markerViewModel.js
@@ -5,6 +5,12 @@ var viewModel = {
 viewModel.markersModel = ko.dependentObservable(function() {
     var self = this;
     var search = self.query().toLowerCase();
+
+    self.loadArticles = function ()
+    {
+        loadArticles(infowindow.location);
+    };
+
     return ko.utils.arrayFilter(markersModel, function(marker) {
         if (marker.name.toLowerCase().indexOf(search) >= 0) {
             return marker.visible(true);
@@ -12,6 +18,8 @@ viewModel.markersModel = ko.dependentObservable(function() {
             setAllMap();
             return marker.visible(false);
         }
+
+
     });
 }, viewModel);
 
